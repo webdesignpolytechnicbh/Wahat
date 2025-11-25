418a14125ee9d7bed88e141eca60c237928df204
webdesignpolytechnicbh <alfardanfatima3@gmail.com>
2025-11-25 21:08:11 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,40 +1,43 @@
 document.getElementsByTagName("tr")[1].onclick = function () {
-  alert("Chicken Sandwich added to cart");
-};
-  
-  document.getElementsByTagName("tr")[2].onclick = function () {
-  alert("Beef Sandwich added to cart");
-};
-  
-  document.getElementsByTagName("tr")[4].onclick = function () {
-  alert("Orange Juice added to cart");
-};
-  
-  document.getElementsByTagName("tr")[5].onclick = function () {
-  alert("Apple Juice added to cart");
+  alert("Club Sandwich added to cart");
 };
 
-  document.getElementsByTagName("tr")[7].onclick = function () {
+document.getElementsByTagName("tr")[2].onclick = function () {
+  alert("Beef Burger added to cart");
+};
+
+
+document.getElementsByTagName("tr")[4].onclick = function () {
+  alert("Orange Juice added to cart");
+};
+
+document.getElementsByTagName("tr")[5].onclick = function () {
+  alert("Banana Juice added to cart");
+};
+
+
+document.getElementsByTagName("tr")[7].onclick = function () {
   alert("Margherita Pizza added to cart");
 };
-  
-  document.getElementsByTagName("tr")[8].onclick = function () {
-  alert("Pepperoni Pizza added to cart");
+
+document.getElementsByTagName("tr")[8].onclick = function () {
+  alert("Veggie Pizza added to cart");
 };
-  
-  document.getElementsByTagName("tr")[10].onclick = function () {
+
+
+document.getElementsByTagName("tr")[10].onclick = function () {
   alert("Pen added to cart");
 };
 
-  document.getElementsByTagName("tr")[11].onclick = function () {
+document.getElementsByTagName("tr")[11].onclick = function () {
   alert("Notebook added to cart");
 };
-  
-  document.getElementsByTagName("tr")[12].onclick = function () {
+
+document.getElementsByTagName("tr")[12].onclick = function () {
   alert("Highlighter added to cart");
 };
-  
-  document.getElementsByClassName("cart-form")[0].onsubmit = function () {
-  alert("Your order has been recieved!");
+
+document.getElementsByClassName("cart-form")[0].onsubmit = function () {
+  alert("Your order has been received!");
   return true;
 };
