6bfda8f8ae22d6872941875d2a4045028df82771
webdesignpolytechnicbh <alfardanfatima3@gmail.com>
2025-12-07 20:54:30 +0300

    Update script.js

--- script.js
+++ script.js
@@ -4,30 +4,3 @@ form.onsubmit = function () {
   alert("Your order has been received!");
   return true; // allow form submission
 };
-document.getElementById("price1").onclick = function() {
-    alert("All prices are subject to VAT");
-};
-document.getElementById("price2").onclick = function() {
-    alert("All prices are subject to VAT");
-};
-document.getElementById("price3").onclick = function() {
-    alert("All prices are subject to VAT");
-};
-document.getElementById("price4").onclick = function() {
-    alert("All prices are subject to VAT");
-};
-document.getElementById("price5").onclick = function() {
-    alert("All prices are subject to VAT");
-};
-document.getElementById("price6").onclick = function() {
-    alert("All prices are subject to VAT");
-};
-document.getElementById("price7").onclick = function() {
-    alert("All prices are subject to VAT");
-};
-document.getElementById("price8").onclick = function() {
-    alert("All prices are subject to VAT");
-};
-document.getElementById("price9").onclick = function() {
-    alert("All prices are subject to VAT");
-};
