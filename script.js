acff37211b2fe0dc0c806fe6f111adee25a8f2c4
webdesignpolytechnicbh <alfardanfatima3@gmail.com>
2025-12-07 21:12:31 +0300

    Update script.js

--- script.js
+++ script.js
@@ -4,8 +4,3 @@ form.onsubmit = function () {
   alert("Your order has been received!");
   return true; // allow form submission
 };
-<script>
-document.getElementById("sandwich-price-header").onclick = function() {
-    alert("All prices are subject to VAT");
-};
-</script>
